c1cc70dfca88e85d865c95219306c276f9617438
Priyanshukcah <priyanshubaghel164@gmail.com>
2026-09-01 22:04:32 +0530

    Refactor equation solver into a separate function
    Refactor equation solver to use a function for better structure and maintainability.

--- equatiopn_solver.cpp
+++ equatiopn_solver.cpp
@@ -1,35 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-  int main()
+vector<double>eqn_solver(int no_of_var,int no_of_equat, vector<vector<double>>mat)
 {
-  // give no of variables then space then number of equations
-  int no_of_var,no_of_equat;
-  cin>>no_of_var>>no_of_equat;
-  vector<vector<float>>mat(no_of_equat,vector<float>(no_of_var+1));
-
-  //Enter each equation line by line(also enter the constant term of the equation)
-  for(int i=0;i<no_of_equat;i++)
-  for(int j=0;j<no_of_var+1;j++)
-  cin>>mat[i][j]; 
-
-  // now we have to solve the system of equations(but assume well defined solution)
+  (but assume well defined solution)
   //the downfall
   for(int i=0;i<no_of_equat-1;i++)//selecting the main row
   {
-    if(mat[i][i]==0)// swapping need check
-    {
+    //always the largest should be taken
+      double maxi=abs(mat[i][i]);
+      int ind =i;
       for(int l=i+1;l<no_of_equat;l++)
-      if(mat[l][i]!=0)
       {
-        for(int z=0;z<no_of_var+1;z++)
+        if(maxi<abs(mat[l][i]))
         {
-          swap(mat[i][z],mat[l][z]);
+            maxi=abs(mat[l][i]);
+            ind=l;
         }
       }
-    }
+      // swapping to ensure blow up does not happen  
+      for(int z=0;z<no_of_var+1;z++)
+        {
+          swap(mat[i][z],mat[ind][z]);
+        }
+      
   for(int j=i+1;j<no_of_equat;j++)// selecting its lower rows
   {
-    float factor = mat[j][i]/mat[i][i];
+    double factor = mat[j][i]/mat[i][i];
     for(int z=0;z<no_of_var+1;z++)
     {
       mat[j][z]=mat[j][z]-factor*mat[i][z];
@@ -41,23 +35,13 @@ using namespace std;
   {
     for(int j=i-1;j>=0;j--)
     {
-      float factor=mat[j][i]/mat[i][i];
+      double factor=mat[j][i]/mat[i][i];
       mat[j][i]=0;
       mat[j][no_of_var]-=factor*mat[i][no_of_var]; 
     }
   }
-
-  //output of matrix
-  // for(int i=0;i<no_of_equat;i++)
-  // {
-  // for(int j=0;j<no_of_var+1;j++)
-  // {
-  //   cout<<mat[i][j]<<" ";
-  // }
-  // cout<<"\n";
-  // }
-  
+  vector<double>ans(no_of_var);
   //final answer
-  for(int i=0;i<no_of_var;i++)cout<<mat[i][no_of_var]/mat[i][i]<<" | ";
-  return 0;
+  for(int i=0;i<no_of_var;i++)ans[i]=mat[i][no_of_var]/mat[i][i];;
+  return ans;
 }
